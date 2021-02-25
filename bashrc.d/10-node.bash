bb890213de80e9cdbdf318771a1eea5b389fcb46
George Song <george@monozuku.com>
2021-02-25 05:58:27 -0800

    Update dependencies and settings

--- bashrc.d/10-node.bash
+++ bashrc.d/10-node.bash
@@ -3,7 +3,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"
 
 # [[ -d $NODE_PATH_ ]] && export PATH=$PATH:$NODE_PATH_
-hash npx 2>/dev/null && source <(npx --shell-auto-fallback bash)
 hash npm 2>/dev/null && source <(npm completion)
 # For compiling the canvas npm package
 export PKG_CONFIG_PATH=/usr/local/opt/libffi/lib/pkgconfig
