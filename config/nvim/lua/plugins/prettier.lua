714f8fe1558fc5a4293a1d5bda07e43507c22558
George Song <george@damacy.net>
2024-01-05 07:25:18 -0800

    Update neovim config

--- config/nvim/lua/plugins/prettier.lua
+++ config/nvim/lua/plugins/prettier.lua
@@ -1,19 +1,4 @@
 return {
-  -- {
-  --   "williamboman/mason.nvim",
-  --   opts = function(_, opts)
-  --     table.insert(opts.ensure_installed, "prettier")
-  --   end,
-  -- },
-  -- {
-  --   "nvimtools/none-ls.nvim",
-  --   optional = true,
-  --   opts = function(_, opts)
-  --     local nls = require("null-ls")
-  --     opts.sources = opts.sources or {}
-  --     table.insert(opts.sources, nls.builtins.formatting.prettier)
-  --   end,
-  -- },
   {
     "stevearc/conform.nvim",
     optional = true,
